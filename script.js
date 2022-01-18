840b4488e1596ab4d4d7ee93de6279d174b11d04
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-18 14:26:10 +0100

    Mein Onlineshop

---
+++ script.js
@@ -0,0 +1,122 @@
+
+function zoomIn1() {
+    console.log("test")
+    document.getElementById("product_1").style.width ="650px";
+    document.getElementById("product_1").style.height = "650px";
+}
+function zoomOut1(){
+     document.getElementById("product_1").style.width = "";
+     document.getElementById("product_1").style.height = "";
+}
+
+
+function zoomIn2() {
+    document.getElementById("product_2").style.width = "650px";
+    document.getElementById("product_2").style.height = "650px";
+}
+function zoomOut2() {
+    document.getElementById("product_2").style.width = "";
+    document.getElementById("product_2").style.height = "";
+}
+
+
+function zoomIn3() {
+    document.getElementById("product_3").style.width = "650px";
+    document.getElementById("product_3").style.height = "650px";
+}
+function zoomOut3() {
+    document.getElementById("product_3").style.width = "";
+    document.getElementById("product_3").style.height = "";
+}
+
+
+function zoomIn4() {
+    document.getElementById("product_4").style.width = "650px";
+    document.getElementById("product_4").style.height = "650px";
+}
+function zoomOut4() {
+    document.getElementById("product_4").style.width = "";
+    document.getElementById("product_4").style.height = "";
+}
+
+
+function zoomIn5() {
+    document.getElementById("product_5").style.width = "650px";
+    document.getElementById("product_5").style.height = "650px";
+}
+function zoomOut5() {
+    document.getElementById("product_5").style.width = "";
+    document.getElementById("product_5").style.height = "";
+}
+
+
+function zoomIn6() {
+    document.getElementById("product_6").style.width = "650px";
+    document.getElementById("product_6").style.height = "650px";
+}
+function zoomOut6() {
+    document.getElementById("product_6").style.width = "";
+    document.getElementById("product_6").style.height = "";
+}
+
+
+function zoomIn7() {
+    document.getElementById("product_7").style.width = "650px";
+    document.getElementById("product_7").style.height = "650px";
+}
+function zoomOut7() {
+    document.getElementById("product_7").style.width = "";
+    document.getElementById("product_7").style.height = "";
+}
+
+
+function zoomIn8() {
+    document.getElementById("product_8").style.width = "650px";
+    document.getElementById("product_8").style.height = "650px";
+}
+function zoomOut8() {
+    document.getElementById("product_8").style.width = "";
+    document.getElementById("product_8").style.height = "";
+}
+
+
+function zoomIn9() {
+    document.getElementById("product_9").style.width = "650px";
+    document.getElementById("product_9").style.height = "650px";
+}
+function zoomOut9() {
+    document.getElementById("product_9").style.width = "";
+    document.getElementById("product_9").style.height = "";
+}    
+
+
+
+
+function kaufen1(){
+    alert("Dieses Product wurde leider ausverkauft!")
+}
+function kaufen2(){
+    alert("Wollen Sie auch eine geschenk dabei?")
+}
+function kaufen3(){
+    alert("Diese Modell hat verschiedene Farben,welche farbe gefält Ihnen?")
+}
+function kaufen4(){
+    alert("Sie haben Glück,denn dies ist das neueste Produkt dieses Modells!")
+}
+function kaufen5(){
+    alert("Dise Product wurde leider ausverkauft")
+}
+function kaufen6(){
+    alert("Möchten Sie diese Product in Ihren Warenkorb lagen?")
+}
+function kaufen7(){
+    alert("Leider ist es nicht verfügbar!")
+}
+function kaufen8(){
+    alert("Möchten Sie männnliches oder webliches Model?")
+}
+function kaufen9(){
+    alert("Sie habenGlück, diese Product reduziert ist!")
+}
+
